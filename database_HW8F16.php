b9abd4032710565b0a5c6fe630db400fd565c80b
zhou0745 <zhou0745@umn.edu>
2016-11-15 20:10:46 -0600

    initalize provided code

---
+++ database_HW8F16.php
@@ -0,0 +1,7 @@
+<?php
+      $db_servername = 'egon.cs.umn.edu';
+      $db_port = 3307;
+      $db_name = 'C4131F16Uxxx';  //Note xxx is a number, you can find it on moodle
+      $db_username = 'C4131F16Uxxx';
+      $db_password = xxxxx;  // This should be on moodle as well.
+?>
