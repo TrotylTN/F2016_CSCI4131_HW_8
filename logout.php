e7966b83227f913e92cea6b48ecc798d4b7df33d
zhou0745 <zhou0745@umn.edu>
2016-11-15 21:07:49 -0600

    updated all websites data from the previous hw

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+      $db_servername = 'egon.cs.umn.edu';
+      $db_port = 3307;
+      $db_name = 'C4131F16U128';  //Note xxx is a number, you can find it on moodle
+      $db_username = 'C4131F16U128';
+      $db_password = 18606;  // This should be on moodle as well.
+?>
